575651eeb99bbee347b49e84ba44eeeb744f038e
CORPORATE\bjf0323 <brad.francis@daugherty.com>
2018-03-20 14:07:14 -0500

    interactive map

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Map} from './map/Map.js';
 
 class App extends Component {
   render() {
@@ -8,11 +9,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Interactive Map Demo</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="map-container">
+          <Map name="Demo" />
+        </div> 
       </div>
     );
   }
